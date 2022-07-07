5f8ddb9caaa0db2a9836bac3de7b06d37ca2b7cf
LwaziV <lwaziabednego@gmail.com>
2022-07-07 16:07:41 +0200

    added capacitor

---
+++ ui/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'running.hill.assessment',
+  appName: 'running-hill-assessment',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
